16429eddbfa776e6db4fd1d7bb70871f6dcef9ce
Cole Peters <cole@colepeters.com>
2013-10-27 00:04:13 +0100

    Let's make those input classes a little more semantic, yeah?

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,8 @@
-$(function(){
-    $('input.convert').click(convert);
+$('input.convert').click(convert);
 
-    function convert() {
-        var input   =   $('input.input').val();
-        var colours =   input.split(' ');
-        var output  =   new Array((colours[0] / 255).toFixed(3).replace('.000',''), (colours[1] / 255).toFixed(3).replace('.000',''), (colours[2] / 255).toFixed(3).replace('.000',''));
-        alert(output);
-    }
-});
+function convert() {
+    var input   =   $('input.swatchInput').val();
+    var colours =   input.split(' ');
+    var output  =   new Array((colours[0] / 255).toFixed(3).replace('.000',''), (colours[1] / 255).toFixed(3).replace('.000',''), (colours[2] / 255).toFixed(3).replace('.000',''));
+    $('input.swatchOutput').val(output[0] + ' ' + output[1] + ' ' + output[2]);
+}
