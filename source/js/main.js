ca4efd1f258eec6b855908c5a7bb760811e76355
Cole Peters <cole@colepeters.com>
2013-10-26 23:45:07 +0100

    Core colour conversion from integer to float, testing with alert

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,10 @@
+$(function(){
+    $('input.convert').click(convert);
 
+    function convert() {
+        var input   =   $('input.input').val();
+        var colours =   input.split(' ');
+        var output  =   new Array((colours[0] / 255).toFixed(3).replace('.000',''), (colours[1] / 255).toFixed(3).replace('.000',''), (colours[2] / 255).toFixed(3).replace('.000',''));
+        alert(output);
+    }
+});
