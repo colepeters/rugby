6e1cc6b7b09e7d7d927d79f5659781680e84f90c
Cole Peters <cole@colepeters.com>
2013-10-27 15:23:44 +0000

    Add a function to change text colour of input to colour being typed
    Signed-off-by: Cole Peters <cole@colepeters.com>

--- source/js/main.js
+++ source/js/main.js
@@ -6,3 +6,22 @@ function convert() {
     var output  =   new Array((colours[0] / 255).toFixed(3).replace('.000',''), (colours[1] / 255).toFixed(3).replace('.000',''), (colours[2] / 255).toFixed(3).replace('.000',''));
     $('input.swatchOutput').val(output[0] + ' ' + output[1] + ' ' + output[2]);
 }
+
+// Swatch input/output text colourisation
+$(function(){
+    $('input.swatchInput').keyup(function(){
+        // Split the input value into a space-delimited array
+        var value = $(this).val().split(' ');
+
+        // If the values haven't been typed yet, default to 0
+        if (value[0] == null) { value[0] = 0; }
+        if (value[1] == null) { value[1] = 0; }
+        if (value[2] == null) { value[2] = 0; }
+
+        // Make 'inputColor' an rgb() swatch using the values typed
+        inputColour = 'rgb(' + value[0] + ',' + value[1] + ',' + value[2] + ')';
+
+        // Change the input text colour to the colour being typed  
+        $(this).css('color', inputColour);
+    });
+});
