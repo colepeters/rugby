d80ba16bf3cd062bebf4d9f08980ae3c7aaaf753
Cole Peters <cole@colepeters.com>
2013-11-03 12:26:32 +0000

    Convert output to textarea

--- source/js/main.js
+++ source/js/main.js
@@ -4,7 +4,7 @@ function convert() {
     var input   =   $('input.swatchInput').val();
     var colours =   input.split(' ');
     var output  =   new Array((colours[0] / 255).toFixed(3).replace('.000',''), (colours[1] / 255).toFixed(3).replace('.000',''), (colours[2] / 255).toFixed(3).replace('.000',''));
-    $('input.swatchOutput').val(output[0] + ' ' + output[1] + ' ' + output[2]);
+    $('textarea.swatchOutput').val(output[0] + ' ' + output[1] + ' ' + output[2]);
 }
 
 // Swatch input/output text colourisation
